c1bdeb5df647f05880085ef73b90f6ef157874b1
Andrei Burd <bandrei@yotpo.com>
2017-07-20 12:12:52 +0300

    no apt

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name             'neo4j'
 maintainer       'Andrei Burd'
 maintainer_email 'bandrei@yotpo.com'
-license          'All right! Reserved.'
+license          'MIT'
 description      'Installs/Configures neo4j'
 long_description 'Installs/Configures neo4j'
-version          '0.1.1'
+issues_url       'https://github.com/YotpoLtd/neo4j-cookbook/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/YotpoLtd/neo4j-cookbook' if respond_to?(:source_url)
+chef_version     '>= 12.5' if respond_to?(:chef_version)
+supports         'ubuntu'
+version          '0.1.2'
 
-depends 'apt', '= 4.0.2'
